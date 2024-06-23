136603e15acb62d54b63460473e43cd5ca462c21
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-06-23 15:24:04 +0600

    Limitations Of One-Way Data Binding In React.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 
+import { useState } from 'react';
 import './App.css'
-import CounterWithClass from './components/CounterWithClassComponent'
 import CounterWithClassFunComponent from './components/CounterWithClassFunComponent'
 
 function App() {
- 
+  const [count, setCount] = useState(0);
   return (
-    <>
-     <CounterWithClass/>
-     <CounterWithClassFunComponent/>
-    </>
+    <div className='border border-purple-500 p-10 m-10'>
+      <h1>Count = {count}</h1>
+     <CounterWithClassFunComponent count={count} setCount={setCount}/>
+    </div>
   )
 }
 
--- src/components/CounterWithClassFunComponent.tsx
+++ src/components/CounterWithClassFunComponent.tsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import ChildComponents from "./ChildComponents";
 
-const CounterWithClassFunComponent = () => {
-    const [count, setCount] = useState(0);
-  return <button onClick={ () => setCount((prev) => prev + 1)}>{count}</button>
+type TProps = {
+  count: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+};
+const CounterWithClassFunComponent = ({ count, setCount }: TProps) => {
+  return (
+    <div className="border border-purple-500 p-10 m-10">
+      <button
+        className="bg-purple-500 p-5 rounded-md border border-black"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        click for update
+      </button>
+      <ChildComponents count={count} />
+    </div>
+  );
 };
 
 export default CounterWithClassFunComponent;
