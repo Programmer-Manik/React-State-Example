1c3a4863e344621b36baa6da8e497aa690a39f62
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-06-24 16:27:07 +0600

    Dynamic Payload And Proper Usage With TypeScript

--- src/components/UserInfoWithUseReducer.tsx
+++ src/components/UserInfoWithUseReducer.tsx
@@ -1,16 +1,23 @@
-import { useReducer } from "react";
+import { ChangeEvent, useReducer } from "react";
 
+type TAction = {
+  type : string;
+  payload: string;
+};
 const initialState = {
   name: "",
   age: "",
-  hobbies: [],
+  hobbies: [] as string[],
 }
 
-const reducer = (currentState, action) => {
+const reducer = (currentState:typeof initialState, action: TAction) => {
   switch (action.type) {
     case 'addName':
-      return {...currentState, name: action.name}
-      
+      return {...currentState, name: action.payload}
+    case 'addAge':
+      return {...currentState, age: action.payload}
+    case 'addHobbies':
+      return {...currentState, hobbies:[...currentState.hobbies, action.payload]}
     default:
       return currentState;
   }
@@ -21,10 +28,15 @@ const UserInfoWithUseReducer = () => {
 const [state, dispatch] = useReducer(reducer, initialState);
 console.log(state)
 
+const handleSubmit = (e:ChangeEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  console.log(state)
+};
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
     <input
-    onChange={(e) => dispatch({type:'addName', name:e.target.value})}
+    onChange={(e) => dispatch({type:'addName', payload:e.target.value})}
     className="border border-purple-300 m-10"
     type="text"
     name="name"
@@ -32,6 +44,7 @@ console.log(state)
     placeholder="name"
     />
     <input
+    onChange={(e) => dispatch({type:'addAge', payload:e.target.value})}
     className="border border-purple-300 m-10"
     type="number"
     name="age"
@@ -39,6 +52,7 @@ console.log(state)
     placeholder="age"
     />
     <input
+    onBlur={(e) => dispatch({type:'addHobbies', payload:e.target.value})}
     className="border border-purple-300 m-10"
     type="text"
     name="hobbies"
